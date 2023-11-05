da234e98be549ecc5bdf4fb9f7d105a76b73f6b0
A-Ionascu <128208422+A-Ionascu@users.noreply.github.com>
2023-11-05 14:07:53 +0200

    Add files via upload

---
+++ Linux_setup.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+sudo apt-get -y install libcurl4-openssl-dev
+
+sudo apt-get -y install libxml2-dev
+
+sudo apt-get -y install cmake
+
+sudo apt-get -y install libfftw3-dev
+
+sudo apt-get -y install libsndfile1-dev
+
+sudo apt-get -y install libssl-dev
